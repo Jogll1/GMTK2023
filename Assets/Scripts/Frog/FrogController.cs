378f63992b1d0a3529b651c9c198d931ecb6fceb
JoelLuckett <87275635+JoelLuckett@users.noreply.github.com>
2023-07-08 09:32:48 +0100

    moving towards goal in a froggy way

--- Assets/Scripts/Frog/FrogController.cs
+++ Assets/Scripts/Frog/FrogController.cs
@@ -7,8 +7,13 @@ public class FrogController : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform targetPos;
 
+    //timer
     public float moveTimer = 0.5f;
-    public float currentMoveTimer;
+    private float currentMoveTimer;
+
+    //goal and car
+    public GameObject player;
+    public GameObject goal;
 
     //rigidbody
     private Rigidbody2D rb;
@@ -49,33 +54,62 @@ public class FrogController : MonoBehaviour
             //reset timer once at target
             currentMoveTimer = moveTimer;
 
-            //random moves for now
-            int ran = Random.Range(0, 2);
-            int h = (ran == 0) ? Random.Range(-1, 2) : 0;
-            int v = (ran == 1) ? Random.Range(-1, 2) : 0;
+            // int ran = Random.Range(0, 2);
+            // int h = (ran == 0) ? Random.Range(-1, 2) : 0;
+            // int v = (ran == 1) ? Random.Range(-1, 2) : 0;
 
-            Vector3 updatedTargetPos = targetPos.position;
+            //moving towards goal
+            int h = 0;
+            int v = 0;
+            //distance between frog and goal
+            float distX = goal.transform.position.x - transform.position.x;
+            float distY = goal.transform.position.y - transform.position.y;
+            Debug.Log(distX + " " + distY);
 
-            if (Mathf.Abs(h) == 1f)
+            int ran = Random.Range(0, 2); //bit of random
+
+            if (ran == 0)
             {
-                //constrain screen size
-                if (updatedTargetPos.x + h <= halfScreenWidth && updatedTargetPos.x + h >= -halfScreenWidth)
+                if (Mathf.Abs(distX) > 0.05f)
                 {
-                    updatedTargetPos.x += h;
+                    h = (int)Mathf.Sign(distX);
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(distY) > 0.05f)
+                {
+                    v = (int)Mathf.Sign(distY);
                 }
             }
 
-            if (Mathf.Abs(v) == 1f)
-            {
-                //constrain screen size
-                if (updatedTargetPos.y + v <= halfScreenHeight && updatedTargetPos.y + v >= -halfScreenHeight)
-                {
-                    updatedTargetPos.y += v;
-                }
-            }
-
-            targetPos.position = updatedTargetPos;
+            targetPos.position = UpdateTargetPos(targetPos.position, h, v);
         }
+    }
+
+    private Vector3 UpdateTargetPos(Vector3 targetPos, int h, int v)
+    {
+        Vector3 updatedTargetPos = targetPos;
+
+        if (Mathf.Abs(h) == 1f)
+        {
+            //constrain screen size
+            if (updatedTargetPos.x + h <= halfScreenWidth && updatedTargetPos.x + h >= -halfScreenWidth)
+            {
+                updatedTargetPos.x += h;
+            }
+        }
+
+        if (Mathf.Abs(v) == 1f)
+        {
+            //constrain screen size
+            if (updatedTargetPos.y + v <= halfScreenHeight && updatedTargetPos.y + v >= -halfScreenHeight)
+            {
+                updatedTargetPos.y += v;
+            }
+        }
+
+        return updatedTargetPos;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
